adba2a384ecb3f56e0bb78c44629377d66b3d9a0
Adam Hamsik <haaaad@gmail.com>
2026-03-13 17:12:58 +0100

    Commit prompts, sites evetything to clipsmit

--- Clipsmith/Views/Settings/PromptLibrarySettingsSection.swift
+++ Clipsmith/Views/Settings/PromptLibrarySettingsSection.swift
@@ -12,7 +12,7 @@ struct PromptLibrarySettingsSection: View {
     // MARK: - Sync State
 
     @AppStorage(AppSettingsKeys.promptLibraryURL)
-    private var promptLibraryURL: String = "https://raw.githubusercontent.com/haad/flycut-prompts-library/refs/heads/gh-pages/prompts.json"
+    private var promptLibraryURL: String = "https://haad.github.io/Clipsmith/prompts/prompts.json"
 
     @AppStorage(AppSettingsKeys.promptLibraryLastSync)
     private var lastSyncISO: String = ""
